e1aa24fe38d59b35d5080b3558bfab67de9d37ae
jchenDDD <86749911+jchenDDD@users.noreply.github.com>
2022-11-07 16:47:11 -0800

    commit 2

---
+++ src/main/java/FileReader/GameFileReader_interface.java
@@ -0,0 +1,15 @@
+package FileReader;
+
+import org.json.simple.parser.ParseException;
+
+import java.io.IOException;
+import java.util.HashMap;
+
+public interface GameFileReader_interface {
+    public HashMap<String, Object>[] getEntityData(String dir) throws IOException, ParseException;
+    public HashMap<String, Object>[] getPlayerData() throws IOException, ParseException;
+    public HashMap<String, Object>[] getMonsterData() throws IOException, ParseException;
+    public HashMap<String, Object>[] getCollectibleData() throws IOException, ParseException;
+    public HashMap<String, Object>[] getEquipmentData() throws IOException, ParseException;
+    public Object[][] getMapData() throws IOException, ParseException;
+}
