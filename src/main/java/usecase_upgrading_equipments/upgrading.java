5e38dece91875f196523022a7b89d16db5859f85
Haoyang Cui <haoyang.cui@mail.utoronto.ca>
2022-11-14 04:35:30 -0500

    version 0.2

--- src/main/java/usecase_upgrading_equipments/upgrading.java
+++ src/main/java/usecase_upgrading_equipments/upgrading.java
@@ -1,8 +1,11 @@
 package usecase_upgrading_equipments;
 
+import Interface.InputBoundary;
+import Interface.Visual;
+import controller.Controller;
 import entity.Collectible;
-import entity.Equipment;
 import entity.Player;
+import presenter.Presenter_bottom;
 import usecase_event.Event;
 import usecase_event.NoEvent;
 import usecase_map.Map;
@@ -11,8 +14,7 @@ import java.util.Observable;
 import java.util.Observer;
 
 
-public class upgrading implements Observer {
-
+public class upgrading implements Observer{
     private final Player player;
     private final Map map;
 
@@ -27,73 +29,81 @@ public class upgrading implements Observer {
         Event event;
         event = this.map.getEvent(this.player.getPlayerLocation());
 
-        if (event instanceof NoEvent && arg == "U" || arg == "h") {
+        if (event instanceof NoEvent && arg == "U" || arg == "u") {
             upgrade();
         }
     }
 
 
-
-    private int determine_Ess() {
+    private int determine_Ess(){
         /**
          * Determine the Essence required to do the upgrading. This method will return a non-negative integer.
          *
          * I will set the initial value as 1 here.In the future, I hope I can let the difficulty of the game decide the
-         * collectible items required for healing.
+         * collectible items required for upgrading.
          *
          */
         int i;
         i = 1;
-        //i = get_difficulty()
         return i;
     }
 
-    private int determine_Art() {
+    private int determine_Art(){
         /**
          * Determine the Artifact required to do the upgrading. This method will return a non-negative integer.
          *
          * I will set the initial value as 1 here.In the future, I hope I can let the difficulty of the game decide the
-         * collectible items required for healing.
+         * collectible items required for upgrading.
          *
          */
         int i;
         i = 1;
-        //i = get_difficulty()
         return i;
     }
 
-    private boolean able_to_upgrade(int Essence_have, int Artifact_have, int Essence_need, int Artifact_need) {
+    private boolean able_to_upgrade(int Essence_have, int Artifact_have, int Essence_need, int Artifact_need){
         /**
          *A small helper function
          */
-        if (Essence_need > Essence_have || Artifact_need > Artifact_have) {
+        if (Essence_need>Essence_have || Artifact_need>Artifact_have){
             return false;
         }
         return true;
     }
 
-    private void upgrade() {
-        int Essence_need;
-        int Artifact_need;
-        Collectible Essence;
-        Collectible Artifact;
-        boolean able_to_upgrade;
-        boolean decide_to_upgrade;
-        Essence = player.getCollectible("Essence");
-        Artifact = player.getCollectible("Artifact");
-        Essence_need = determine_Ess();
-        Artifact_need = determine_Art();
-        able_to_upgrade() = able_to_upgrade(Essence.getNum(), Artifact.getNum(), Essence_need, Artifact_need);
-        decide_to_upgrade = popup_window_upgrade(Essence.getNum(), Artifact.getNum(), Essence_need, Artifact_need,
-                able_to_upgrade);
-        if (decide_to_upgrade) {
-            player.changeCurrHitPoint(player.getMaxHitPoint());
-            player.changeCollectibleAmount("Essence", Essence_need);
-            player.changeCollectibleAmount("Artifact", Artifact_need);
+    private String info_writer(int Essence_have, int Essence_need, int Artifact_have, int Artifact_need){
+        if(able_to_upgrade(Essence_have,Artifact_have,Essence_need,Artifact_need)) {
+            return "You have Essence " + Integer.toString(Essence_have) + "/" + Integer.toString(Essence_need)
+                    + ". /n You have Artifact " + Integer.toString(Artifact_have) + "/" +
+                    Integer.toString(Artifact_need) + "/n You can upgrade! /n Upgrading? [Y]/[N]";
         }
+        return "You have Essence " + Integer.toString(Essence_have) + "/" + Integer.toString(Essence_need)
+                + ". /n You have Artifact " + Integer.toString(Artifact_have) + "/" +
+                Integer.toString(Artifact_need) + "/n You cannot upgrade! /n Try to grt more collections!";
     }
 
-    ;
 
+    private void upgrade() {
+        /**
+         * The basic part of the upgrading, it will return nothing but send message to presenter.
+         */
+        Visual vision = new Presenter_bottom();
+        InputBoundary input = new Controller();
+        int Essence_need = determine_Ess();
+        int Artifact_need = determine_Art();
+        Collectible Essence = player.getCollectible("Essence");
+        Collectible Artifact = player.getCollectible("Artifact");
+        if (able_to_upgrade(Essence.getNum(), Artifact.getNum(), Essence_need, Artifact_need)) {
+            vision.show_heal_info(info_writer(Essence.getNum(),Essence_need, Artifact.getNum(), Artifact_need));
+            if (input.get_heal_decision()) {
+                player.changeCurrHitPoint(player.getMaxHitPoint());
+                player.changeCollectibleAmount("Essence", Essence_need);
+                player.changeCollectibleAmount("Artifact", Artifact_need);
+                vision.show_heal_info("Your healing success!!! HP is full now!!!");
+                return;
+            }
+            vision.show_heal_info("You choose not to heal. Good Luck!");
+            return;
+        }
+    }
 }
-
