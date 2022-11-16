e275bdb771bd650a419ea9bfc394c35db829db70
Stalight_ <fishy754@gmail.com>
2022-11-15 21:18:24 -0500

    small changes to Player Builder

--- src/main/java/usecase_gamedata/PlayerBuilder.java
+++ src/main/java/usecase_gamedata/PlayerBuilder.java
@@ -1,16 +1,16 @@
-package usecase_gamedata;
-
-import entity.Armor;
-import entity.BasicEquipmentSlots;
-import entity.Weapon;
-import org.json.simple.JSONObject;
-
-public interface PlayerBuilder {
-    public void buildMaxHp(int maxHP);
-    public void buildAtkPt(int atkPt);
-    public void buildLocation(int[] location);
-
-    public void buildEquipmentSlot(JSONObject jsonObject);
-
-    public void buildInventory(GameData gameData);
-}
+//package usecase_gamedata;
+//
+//import entity.Armor;
+//import entity.BasicEquipmentSlots;
+//import entity.Weapon;
+//import org.json.simple.JSONObject;
+//
+//public interface PlayerBuilder {
+//    public void buildMaxHp(int maxHP);
+//    public void buildAtkPt(int atkPt);
+//    public void buildLocation(int[] location);
+//
+//    public void buildEquipmentSlot(JSONObject jsonObject);
+//
+//    public void buildInventory(GameData gameData);
+//}
