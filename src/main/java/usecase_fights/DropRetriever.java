5df36186ee9f1a29ae9f11a858cf9777acf96b96
Jaemi V <j.villanueva@mail.utoronto.ca>
2022-11-19 21:51:07 -0500

    Implemented helper functions to determine what to drop and how many to drop.

--- src/main/java/usecase_fights/DropRetriever.java
+++ src/main/java/usecase_fights/DropRetriever.java
@@ -1,5 +1,50 @@
 package usecase_fights;
 
-/** A Retriever that obtains all the items dropped after a fight. */
+import java.util.Random;
+
+/** A Retriever that obtains the items dropped after a fight. */
 public class DropRetriever {
+    /**
+     * Get the drops of a fight.
+     */
+    public void getDrops(){
+        // determine whether we will drop weapon or armour, essence or artifact
+        boolean dropWeapon = this.toDrop();
+        boolean dropArtifact = this.toDrop();
+        if (dropArtifact){
+            int artifact = this.getCollectibleNum("Artifact");
+        } else{
+            int essence = this.getCollectibleNum("Essence");
+        }
+
+        if(dropWeapon){
+            // create a new weapon
+        } else{
+            // create new armor
+        }
+    }
+
+    /**
+     * @return Whether to drop an Item.
+     */
+    private boolean toDrop() {
+        Random rand = new Random();
+        return rand.nextBoolean(); // randomly decides if Artifact is dropped
+    }
+
+    /**
+     *
+     * @param collectibleType The type of Collectible that will be dropped.
+     * @return The number of Collectibles that will be dropped.
+     */
+    private int getCollectibleNum(String collectibleType){
+        Random rand = new Random();
+        int bound = 0;
+        if(collectibleType.equals("Essence")){
+            bound = 10; // drops 1-10 essence inclusive
+        } else if (collectibleType.equals("Artifact")) {
+            bound = 2; // drops 1-2 artifact inclusive
+        }
+        return rand.nextInt(bound) + 1;
+    }
 }
