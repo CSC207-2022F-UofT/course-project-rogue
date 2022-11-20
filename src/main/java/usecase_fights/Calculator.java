f3e48eba07d3dba80ed673199646259e9a55a6d9
Jaemi V <j.villanueva@mail.utoronto.ca>
2022-11-19 21:44:45 -0500

    Changed Calculator into an abstract class

--- src/main/java/usecase_fights/Calculator.java
+++ src/main/java/usecase_fights/Calculator.java
@@ -1,12 +1,5 @@
 package usecase_fights;
 
-public class Calculator {
-    Calculated calculated;
-    public Calculator(Calculated calculated) {
-        this.calculated = calculated;
-    }
-
-    public int calculateValue(){
-        return this.calculated.calculate();
-    }
+public abstract class Calculator {
+    public abstract int calculate();
 }
