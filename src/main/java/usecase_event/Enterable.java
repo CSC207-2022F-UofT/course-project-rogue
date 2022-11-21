4289cb351e44861a2c558bd32973507d3d2226a0
Stalight_ <fishy754@gmail.com>
2022-11-21 10:34:21 -0500

    Update Enterable.java

--- src/main/java/usecase_event/Enterable.java
+++ src/main/java/usecase_event/Enterable.java
@@ -5,6 +5,8 @@ import entity.Player;
 public interface Enterable {
     /**
      * Return whether player can enter this Event Tile
+     *
+     * Note: Player interface is there to increase flexibility because there may be a Room that needs a key from Player
      */
     boolean enter(Player player);
 }
