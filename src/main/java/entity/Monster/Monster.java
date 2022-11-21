f8928be6fb7ef156a13fd8626d37346f423da126
Jaemi V <j.villanueva@mail.utoronto.ca>
2022-11-21 05:03:12 -0500

    Changed variable type of power from Power -> MonsterPower

--- src/main/java/entity/Monster/Monster.java
+++ src/main/java/entity/Monster/Monster.java
@@ -26,7 +26,7 @@ public class Monster extends Character {
     private final boolean HAS_POWER;
 
     /** The power of the Monster. Null if hasPower is false. */
-    private Power power; // not final to allow for extension of changing powers
+    private MonsterPower power; // not final to allow for extension of changing powers
 
 
     /**
@@ -58,7 +58,7 @@ public class Monster extends Character {
      *
      * @param pwr The power of Monster.
      */
-    public Monster(String n, String t, HashMap<String, int[]> stats, boolean state, Power pwr) {
+    public Monster(String n, String t, HashMap<String, int[]> stats, boolean state, MonsterPower pwr) {
         this(n, t, stats, state);
         this.power = pwr;
     }
@@ -103,7 +103,7 @@ public class Monster extends Character {
     /**
      * @return The Power of the Monster.
      */
-    public Power getPower(){
+    public MonsterPower getPower(){
         return this.power;
     }
 
