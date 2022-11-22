d959b93bfc706fc3af5755a9ffa09ea8a300fc2b
jchenDDD <86749911+jchenDDD@users.noreply.github.com>
2022-11-21 18:38:13 -0800

    commit player test case

--- src/test/java/file_reader_tests/GameFileReaderTest.java
+++ src/test/java/file_reader_tests/GameFileReaderTest.java
@@ -1,7 +1,7 @@
 package file_reader_tests;
 
-import FileReader.GameFileReader;
-import FileReader.GameFileReader_interface;
+import file_reader.GameFileReader;
+import file_reader.GameFileReaderInterface;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import entity.Player;
@@ -9,24 +9,113 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class GameFileReaderTest {
-    GameFileReader_interface playerFileReader =
-            new GameFileReader("playerFileTest.json");
+    GameFileReaderInterface playerFileReader =
+            new GameFileReader("src/test/java/file_reader_tests/playerFileTest.json");
 
-    GameFileReader_interface monsterFileReader =
-            new GameFileReader("monsterFileTest.json");
+    GameFileReaderInterface monsterFileReader =
+            new GameFileReader("src/test/java/file_reader_tests/monsterFileTest.json");
 
+    //Player 1 to 4 are in top to bottom order in playerFileTest.json
+    //PLEASE READ ME:
+    // For all verify helper methods below, it asserts whether the player read in is the correct one.
+    void verifyPlayer1(Player player){
+        Assertions.assertEquals(100, player.getMaxHitPoint());
+        Assertions.assertEquals(1, player.getAttackPoint());
+        Assertions.assertEquals(12323, player.getEssence().getNum());
+        Assertions.assertEquals(0, player.getArtifact().getNum());
+        Assertions.assertEquals(1223, player.getEquipment("Weapon").getStatValue());
+        Assertions.assertEquals(122, player.getEquipment("Armor").getStatValue());
+    }
+
+    void verifyPlayer4(Player player){
+        Assertions.assertEquals(0, player.getMaxHitPoint());
+        Assertions.assertEquals(0, player.getAttackPoint());
+        Assertions.assertEquals(0, player.getEssence().getNum());
+        Assertions.assertEquals(0, player.getArtifact().getNum());
+        Assertions.assertEquals(1, player.getEquipment("Weapon").getStatValue());
+        Assertions.assertEquals(0, player.getEquipment("Armor").getStatValue());
+    }
+
+    void verifyPlayer3(Player player){
+        Assertions.assertEquals(41, player.getMaxHitPoint());
+        Assertions.assertEquals(2, player.getAttackPoint());
+        Assertions.assertEquals(-23, player.getEssence().getNum());
+        Assertions.assertEquals(0, player.getArtifact().getNum());
+        Assertions.assertEquals(4, player.getEquipment("Weapon").getStatValue());
+        Assertions.assertEquals(5, player.getEquipment("Armor").getStatValue());
+    }
+
+    void verifyPlayer2(Player player){
+        Assertions.assertEquals(1, player.getMaxHitPoint());
+        Assertions.assertEquals(512, player.getAttackPoint());
+        Assertions.assertEquals(234, player.getEssence().getNum());
+        Assertions.assertEquals(0, player.getArtifact().getNum());
+        Assertions.assertEquals(2, player.getEquipment("Weapon").getStatValue());
+        Assertions.assertEquals(122, player.getEquipment("Armor").getStatValue());
+    }
     @Test
     void testBasicPlayerCreation(){
         try {
+            //1st JSONObject in list
             Player player = new ObjectMapper().
                     readValue(playerFileReader.findString("class", "Basic Player"), Player.class);
-            Assertions.assertEquals(100, player.getMaxHitPoint());
-            Assertions.assertEquals(1, player.getAttackPoint());
-            Assertions.assertEquals(12323, player.getE)
+            verifyPlayer1(player);
         } catch (JsonProcessingException e) {
             throw new RuntimeException(e);
         }
+    }
 
+    @Test
+    void testFindString(){
+        try {
+            //4th JSONObject in list
+            Player player = new ObjectMapper().
+                    readValue(playerFileReader.findString("test", "Test"), Player.class);
+            verifyPlayer4(player);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Test
+    void testFindStringEmptyString(){
+        try {
+            //3rd JSONObject in list
+            Player player = new ObjectMapper().
+                    readValue(playerFileReader.findString("class", ""), Player.class);
+            verifyPlayer3(player);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Test
+    void testFindInt(){
+        try {
+            //2nd JSONObject in list
+            Player player = new ObjectMapper().
+                    readValue(playerFileReader.findInt("maxHitPoint", 1), Player.class);
+            verifyPlayer2(player);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Test
+    void testFindBoolean(){
+        try {
+            //3rd JSONObject in list
+            Player player = new ObjectMapper().
+                    readValue(playerFileReader.findBoolean("StraitCurve", true), Player.class);
+            verifyPlayer3(player);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Test
+    void testFindStringValueDoesNotExist(){
+        Assertions.assertNull(playerFileReader.findString("class", "Difficult"));
     }
 
 
