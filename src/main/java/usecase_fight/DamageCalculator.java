fa1a2e4f8d16d769ab54dee0b439728f0359df9d
villanueva-j <113061736+villanueva-j@users.noreply.github.com>
2022-11-23 20:25:52 -0500

    Adjusted damage calculation to consider the number of possible turns in the fight.

--- src/main/java/usecase_fight/DamageCalculator.java
+++ src/main/java/usecase_fight/DamageCalculator.java
@@ -22,17 +22,48 @@ public class DamageCalculator extends Calculator {
     }
 
     /**
-     * Amount of damage dealt is the attack power of the Monster subtracted by the damage reduction of the Player.
-     * (This is temporary as I don't know how you guys want me to balance this).
+     * Calculates the amount of damage dealt to Player in a fight.
+     * Total damage is determined by: Number of turns * Monster's damage per turn
+     *              -> where the number of turns is the number of hits required for Player to kill Monster.
+     *              -> where damage per turn is determined by: Monster attack - Player damage reduction.
      * If the damage reduction of the Player is greater than attack power of Monster, return 0.
      *
-     * @return The amount of damage that this monster will inflict.
+     * @return The total amount of damage that this monster will inflict.
      */
     @Override
     public int calculate() {
         int mAtk = this.monster.getAttack();
         int pRed = this.player.getEquipment("Armor").getStatValue();
-        return Math.max((mAtk - pRed), 0);
+        int attackDmg = Math.max((mAtk - pRed), 0); // attack per turn, minimum of 0
+        // if attackDmg is 0, then return attackDmg
+        if (attackDmg == 0){
+            return attackDmg;
+        } else {  // else, get number of turns
+            int turns = this.getNumberTurns(attackDmg);
+            return attackDmg * turns;
+        }
+    }
+
+    /**
+     * Determines the number of turns in the fight; the number of turns is the lowest number of turns required to kill
+     * either the Player or Monster.
+     *
+     * @param monsterAtk The damage per turn of the Monster. Value is always > 0.
+     * @return The number of turns in the fight.
+     */
+    private int getNumberTurns(int monsterAtk){
+        int pHp = player.getCurrHitPoint();
+        int mHp = monster.getHealth();
+        int pAtk = player.getAttackPoint() + player.getEquipment("Weapon").getStatValue();
+
+        float pHitsRequired = (float) pHp / monsterAtk; // the number hits required to kill player
+
+        if (pAtk <= 0){ // player can never kill monster
+            return Math.round(pHitsRequired); // turns determined by number of hits required to kill player
+        }
+        float mHitsRequired = (float) mHp / pAtk; // the number hits required to kill monster
+
+        return Math.round(Math.min(pHitsRequired, mHitsRequired));
     }
 
     /**
