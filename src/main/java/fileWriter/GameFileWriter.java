584925f0d6965c597bfeabad5452c34cff759853
jchenDDD <86749911+jchenDDD@users.noreply.github.com>
2022-11-25 18:13:01 -0800

    Setting up observable pattern between gameFileWriter and gameFileReader

--- src/main/java/fileWriter/GameFileWriter.java
+++ src/main/java/fileWriter/GameFileWriter.java
@@ -3,18 +3,38 @@ package fileWriter;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import entity.Player;
+import file_reader.GameFileReaderInterface;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class GameFileWriter implements GameFileWriter_Interface{
     private String dir;
+    private GameFileReaderInterface reader;
 
     public GameFileWriter(String dir){
         this.dir = dir;
     }
+
+    /**
+     * @param fileReader : reader to be notified after this writer has written to a file.
+     */
+    @Override
+    public void register(GameFileReaderInterface fileReader) {
+        this.reader = fileReader;
+    }
+
+    /**
+     * Notifying the reader to switch to a new file to read.
+     */
+    @Override
+    public void notifyReader() {
+        this.reader.update(this.dir);
+    }
+
     /**
      * @param player: the player instance that will replace the player instance in Player.json
      */
@@ -56,19 +76,11 @@ public class GameFileWriter implements GameFileWriter_Interface{
             fileWriter.write(arr.toJSONString());
             fileWriter.close();
             System.out.println("Wrote this player to " + this.dir + ": " + pl);
+            this.notifyReader();
         } catch (IOException e) {
             System.out.println("Did not write player to any file");
             throw new RuntimeException(e);
         }
     }
 
-    @Override
-    public void setDir(String dir){
-        this.dir = dir;
-    }
-
-    @Override
-    public String getDir(){
-        return this.dir;
-    }
 }
