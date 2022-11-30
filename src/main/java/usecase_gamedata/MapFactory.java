afb2eb8dbf40751d4132af60ae306168edc655c1
jchenDDD <86749911+jchenDDD@users.noreply.github.com>
2022-11-29 19:19:52 -0800

    Changes in MapFactory to match with Controller from another pull request

--- src/main/java/usecase_gamedata/MapFactory.java
+++ src/main/java/usecase_gamedata/MapFactory.java
@@ -9,24 +9,45 @@ import file_reader.deserialization.DeserializeHelper;
 import game_data.PlayerInstances;
 import usecase_playeractions.Map;
 
+import java.util.HashMap;
+
 public class MapFactory {
 
     private GameFileReaderInterface mf;
 
+    private HashMap<Integer, int[]> startingLocations;
+
+    private int level;
+
     public MapFactory(GameFileReaderInterface mf){
         this.mf = mf;
+        startingLocations = new HashMap<>();
     }
 
-    public Map create(Player player, int level){
+    /**
+     * This method creates a map and record the starting location of the map.
+     * @param level : The index of the map recorded in Map.json
+     * @return A Map instance with the corresponding level value.
+     */
+    public Map create(int level){
         String jsonString = mf.findInt("index", level);
         try {
             Map map = new ObjectMapper().readValue(jsonString, Map.class);
             JsonNode mn = new ObjectMapper().readTree(jsonString);
             int[] location = new DeserializeHelper().readIntArr(mn.get("starting"));
-            player.setLocation(location[0], location[1]);
+            startingLocations.put(level, location);
+            this.level = level;
             return map;
         } catch (JsonProcessingException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    /**
+     * Set the player to have the starting location of the latest map created.
+     * @param player : player of the game
+     */
+    public void setLocation(Player player){
+        player.setLocation(startingLocations.get(level)[0], startingLocations.get(level)[1]);
     }
 }
