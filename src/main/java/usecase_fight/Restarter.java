6e2f7dcd3fb111093d84be5d37b3d6385627091a
villanueva-j <113061736+villanueva-j@users.noreply.github.com>
2022-11-30 14:30:38 -0500

    Added call to output boundary method

--- src/main/java/usecase_fight/Restarter.java
+++ src/main/java/usecase_fight/Restarter.java
@@ -1,12 +1,14 @@
 package usecase_fight;
 
 import entity.player.Player;
+import interface_adapters.OutputBoundary;
 
 import java.util.Observable;
 import java.util.Observer;
 
 /** A restarter that restarts the game from last Player save. */
 public class Restarter implements Observer {
+    private final OutputBoundary outputBoundary;
     private final Player player;
     /** The keystroke that triggers this restarter. */
     private final String trigger; // (Continue key)
@@ -16,7 +18,8 @@ public class Restarter implements Observer {
      * @param player A given Player.
      * @param trigger The string that triggers Restarter to update.
      */
-    public Restarter(Player player, String trigger){
+    public Restarter(OutputBoundary outputBoundary, Player player, String trigger){
+        this.outputBoundary = outputBoundary;
         this.player = player;
         this.trigger = trigger;
     }
@@ -29,7 +32,7 @@ public class Restarter implements Observer {
     @Override
     public void update(Observable o, Object arg) {
         if (player.getGameOver() && trigger.equals(arg)){
-            // go to main menu
+            outputBoundary.updateDead();
         }
     }
 }
